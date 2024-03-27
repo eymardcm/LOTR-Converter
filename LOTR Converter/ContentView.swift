24f7c15da5424685ec84bb356d1cb0a05e2441af
Chad Eymard <eymardcm@gmail.com>
2024-03-26 21:55:56 -0400

    ContentView screen design

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -8,14 +8,126 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showExchangeInfo: Bool = false
+    @State private var leftAmount: String = ""
+    @State private var rightAmount: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Image(.background)
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                
+            VStack(alignment: .center, content: {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    
+                Text("Currency Exchange")
+                    .foregroundStyle(.colorLightBrown)
+                    .font(.largeTitle)
+                    
+                Spacer()
+                    // Currency conversion section
+                HStack {
+                    
+                    Group {
+                        // Left conversion section
+                        VStack {
+                            // Currency
+                            HStack {
+                                // Currency image
+                                Image(.copperpenny)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 33)
+                                
+                                // Currency text
+                                Text("Copper Penny")
+                                    .font(.headline)
+                                    .foregroundStyle(.colorLightBrown)
+                            } //: HSTACK
+                            .padding(.bottom, -3)
+                            
+                            // Left TextField
+                            TextField("Amount", text: $leftAmount)
+                                .modifier(TextFieldModifier())
+                            
+                        } //: VSTACK
+                        
+                        // Equal Sign
+                        Image(systemName: "equal")
+                            .foregroundColor(.colorLightBrown)
+                            .font(.largeTitle)
+                            .symbolEffect(.pulse)
+                        
+                        // Right conversion section
+                        VStack {
+                            // Currency
+                            HStack {
+                                // Currency text
+                                Text("Silver Piece")
+                                    .font(.headline)
+                                    .foregroundStyle(.colorLightBrown)
+                                
+                                // Currency image
+                                Image(.silverpiece)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 33)
+                                
+                            } //: HSTACK
+                            .padding(.bottom, -3)
+                            
+                            // Right TextField
+                            TextField("Amount", text: $rightAmount)
+                                .modifier(TextFieldModifier())
+                                .multilineTextAlignment(.trailing)
+                            
+                        } //: VSTACK
+                    } //: GROUP
+                    .padding(.bottom, 25)
+                    
+                    
+                } //: HSTACK
+                .padding()
+                .background(.black.opacity(0.3))
+                .cornerRadius(20)
+                    
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                    
+                // Info button
+                HStack() {
+                    Spacer()
+                    Button(action: {
+                        showExchangeInfo.toggle()
+                    }, label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.colorLightBrown)
+                    })
+                    .padding(.trailing)
+                    .padding(.bottom, 25)
+                } //: HSTACK
+            }) //: VSTACK
+            .frame(maxWidth: UIScreen.screenWidth)
+        } //: ZSTACK
+        
+    }
+
+}
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .textFieldStyle(.roundedBorder)
+            .cornerRadius(12)
     }
 }
 
---
+++ LOTR Converter/Extensions/SetScreenWidthExtension.swift
@@ -0,0 +1,14 @@
+//
+//  SetScreenWidthExtension.swift
+//  LOTR Converter
+//
+//  Created by Chad Eymard on 3/26/24.
+//
+
+import SwiftUI
+
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+}
